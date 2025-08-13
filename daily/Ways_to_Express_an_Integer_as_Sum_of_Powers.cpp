315b5912e8c03d6c4ac7f7b494de4b0b633f14fd
taipt3 <taipt3@vng.com.vn>
2025-08-13 10:58:53 +0700

    update

--- daily/Ways_to_Express_an_Integer_as_Sum_of_Powers.cpp
+++ daily/Ways_to_Express_an_Integer_as_Sum_of_Powers.cpp
@@ -17,8 +17,8 @@ using namespace std;
 /*
 */
 void solve(){
-	int n=4;
-	int x=1;
+	int n=10;
+	int x=2;
 	
 	int mod = 1e9+7;
 	function<int(long long,int)> power=[&](long long a,int b){
@@ -30,8 +30,12 @@ void solve(){
 		}
 		return result;
 	};
-	vector<vector<int>> mm(301,vector<int>(300,-1));
-	cout<<"aaaa"<<endl;
+
+	vector<int> pw(n+1,0);
+	for(int i =0;i<=n;i++) pw[i] = power(i,x);
+
+
+	vector<vector<int>> mm(301,vector<int>(301,-1));
 	function<int(int,int)> dp=[&](int n,int first){
 		if(mm[n][first]!=-1) return mm[n][first];
 		if(n==0) return 1;
@@ -39,18 +43,50 @@ void solve(){
 		int total = 0;
 		for(int i = first;i <=n;i++){
 			if(power(i,x) <= n){
-				total += dp(n - power(i,x), first+1); 
+				total += dp(n - pw[i], i+1); 
+				total%=mod;
 			}
 		}
 		mm[n][first] = total;
-		cout<<n <<" "<<first<<" "<< total<<endl;
 		return total;
 	};
 	cout<<dp(n,1)<<endl;
 }
+
+void solve2(){
+	int n = 4;
+	int x = 1;
+	int mod = 1e9+7;
+	function<int(long long,int)> power=[&](long long a,int b){
+		long long result = 1;
+		while(b >0){
+			if(b&1) result = (result*a)%mod;
+			a = (a*a)%mod;
+			b>>=1;
+		}
+		return result;
+	};
+	vector<int> pw(n+1,0);
+	for(int i =0;i<=n;i++) pw[i] = power(i,x);
+
+	vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+
+	dp[0][0] = 1;
+
+	for(int j =1;j<=n;j++){
+		for(int i = 0;i<=n;i++){
+			dp[i][j] = dp[i][j-1];
+			if(pw[j]<= i ){
+				dp[i][j]+=dp[i- pw[j]][j-1];
+			} 
+		}
+	}
+
+	cout<< dp[n][n]<<endl;
+}
  
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    solve();
+    solve2();
     return 0;
 }
