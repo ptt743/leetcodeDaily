d02f8a4f8bbff8713a49bad3519bc3bc05362d3a
taipt3 <taipt3@vng.com.vn>
2025-09-16 17:54:59 +0700

    update

--- daily/Find_the_Minimum_Area_to_Cover_All_Ones_II.cpp
+++ daily/Find_the_Minimum_Area_to_Cover_All_Ones_II.cpp
@@ -10,6 +10,7 @@
 #include<unordered_map>
 #include<cmath>
 #include<functional>
+#include <climits>
 #define ll long long
 
 using namespace std;
@@ -17,28 +18,24 @@ using namespace std;
 /*
 */
 void solve(){
-	vector<vector<int>> grid;
-
-	int n = grid.size();
-	int m = grid[0].size();
-
-	function<vector<vector<int>> rotate=[&](){
-		int n = grid.size();
-		int m = grid[0].size();
+	vector<vector<int>> grid = {{1,0,1},{1,1,1}};
+	function<vector<vector<int>>(vector<vector<int>>&)> rotate=[&](vector<vector<int>>& input){
+		int n = input.size();
+		int m = input[0].size();
 		vector<vector<int>> result = vector<vector<int>>(m, vector<int>(n,0));
 		for(int i = 0;i<n;i++){
 			for(int j = 0;j < m;j++){
-				result[j][i] = grid[i][j];
+				result[m - j-1][i] = input[i][j];
 			}
 		}
 		return result;
 	};
 
-	function<int(vector<vector<int>>, int, int, int, int)> cal= [&](, vector<vector<int>>& input, int left, int right, int up, int down){
-		int min_j = right, max_i = 0;
-		int min_i = up, max_j = 0;
-		for(int i =  up, i<=down; i++){
-			for(int j = left , j < right; j++){
+	function<int(vector<vector<int>>&, int, int, int, int)> cal= [&](vector<vector<int>>& input, int left, int right, int up, int down){
+		int min_j = input[0].size(), max_i = 0;
+		int min_i = input.size() , max_j = 0;
+		for(int i =  up; i< down; i++){
+			for(int j = left ; j < right; j++){
 				if(input[i][j] == 1 ){
 					min_i = min(min_i, i);
 					max_i = max(max_i, i);
@@ -47,20 +44,32 @@ void solve(){
 				}
 			}
 		}
-		return ( max_i - min_i) * ( max_j - min_j );
+		int res =  (min_i<=max_i)?(( max_i - min_i +1) * ( max_j - min_j+1)):(INT_MAX/3);
+		return res;
 	};
 
-	function<int()> solve=[&](){
+	function<int(vector<vector<int>>&)> sol=[&](vector<vector<int>>& input){
+		int res  = input.size() * input[0].size();
+		int n = input.size();
+		int m = input[0].size();
 		for(int i = 1;i< n;i++){
-			for(int j = 1; j< m;j++){
-					
+			for(int j = 1; j<m;j++){
+				res = min(res,cal(input, 0, j, 0, i) + cal(input, j, m, 0, i) + cal(input, 0, m, i, n));
+				res = min(res,cal(input, 0, m, 0, i) + cal(input, 0, j, i, n) + cal(input, j, m, i, n));
 			}
 		}
+		for(int i = 1;i<n-1;i++){
+			for( int j = i+1;j< n;j++){
+				res = min(res,cal(input, 0,m, 0,i) + cal(input, 0,m,i,j) + cal(input, 0,m,j,n));
+			}
+		}
+		return res;
+	};
 
-	}
-	
-	
+	vector<vector<int>> rotateGrid = rotate(grid);
 
+	int result = min(sol(grid),sol(rotateGrid));
+	cout<< result <<endl;
 }
  
 int main() {
